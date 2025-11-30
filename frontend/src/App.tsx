4848829e2da764904d5367200868369986d40375
Fawaz Shah <fawaz010@gmail.com>
2025-11-30 23:34:36 +0000

    Initialise WASM on initial render. Add prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import init, { add, greet, base64_encode } from "./wasm/wasm.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [wasmReady, setWasmReady] = useState(false);
+
+  // Initialise WASM on initial render
+  useEffect(() => {
+    init().then(() => setWasmReady(true));
+  }, []);
 
   return (
     <>
@@ -29,32 +36,31 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 
+// < !DOCTYPE html >
+//   <html>
 
-  // < !DOCTYPE html >
-  //   <html>
+//     <body>
+//       <h1>Rust + WebAssembly Demo</h1>
+//       <div id="out"></div>
+//       <div id="encoding"></div>
 
-  //     <body>
-  //       <h1>Rust + WebAssembly Demo</h1>
-  //       <div id="out"></div>
-  //       <div id="encoding"></div>
+//       <script type="module">
+//         import init, {add, greet, base64_encode} from "./wasm/wasm.js";
 
-  //       <script type="module">
-  //         import init, {add, greet, base64_encode} from "./wasm/wasm.js";
+//         async function run() {
+//           await init();
+//         document.getElementById("out").innerText =
+//         greet("World") + " 2+3=" + add(2, 3);
 
-  //         async function run() {
-  //           await init();
-  //         document.getElementById("out").innerText =
-  //         greet("World") + " 2+3=" + add(2, 3);
+//         document.getElementById("encoding").innerText = base64_encode(64);
+//   }
+//         run();
+//       </script>
+//     </body>
 
-  //         document.getElementById("encoding").innerText = base64_encode(64);
-  //   }
-  //         run();
-  //       </script>
-  //     </body>
-
-  //   </html>
+//   </html>

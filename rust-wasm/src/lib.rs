f8f4abe984392266174c3c5badf5d656a3c70002
Fawaz Shah <fawaz010@gmail.com>
2025-12-03 23:09:22 +0000

    Add web-sys for console logs, and panic hook to show Rust errors in console

--- rust-wasm/src/lib.rs
+++ rust-wasm/src/lib.rs
@@ -3,6 +3,11 @@ use std::io::Cursor;
 use image::{DynamicImage, load_from_memory};
 use wasm_bindgen::prelude::wasm_bindgen;
 
+#[wasm_bindgen(start)]
+pub fn start() {
+    console_error_panic_hook::set_once();
+}
+
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
     a + b

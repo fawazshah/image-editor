c311427204c5bdff215adf1af3eb88de765c29d9
Fawaz Shah <fawaz010@proton.me>
2026-02-01 19:57:46 +0000

    Add styled tooltip

--- src/components/StyledClickableCanvas.tsx
+++ src/components/StyledClickableCanvas.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Tooltip } from "react-bootstrap";
 
 export const StyledClickableCanvas = styled.div`
   display: flex;
@@ -8,4 +9,33 @@ export const StyledClickableCanvas = styled.div`
 
 export const StyledButton = styled.button`
   margin: auto;
+`;
+
+export const StyledTooltip = styled(Tooltip)`
+  margin-left: 10px;
+
+  .tooltip-inner {
+    background-color: #f9f9f9;
+    color: black;
+    border: 1px solid #ccc;
+    font-size: medium;
+    white-space: nowrap;
+  }
+
+  /* Outer border of arrow */
+  .tooltip-arrow::before {
+    border-right-color: #ccc;
+  }
+
+  /* Inner fill of arrow (slightly offset to create border effect) */
+  .tooltip-arrow::after {
+    content: "";
+    position: absolute;
+    border-style: solid;
+    border-width: 5px;
+    border-color: transparent #f9f9f9 transparent transparent;
+    left: 2px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
 `;
